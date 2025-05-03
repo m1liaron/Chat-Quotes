f5db0332825699be9a2dcc9fa88603ba135cca79
Vlad <machinker34@gmail.com>
2025-05-03 21:36:45 +0300

    feat: use bacic express middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,11 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
 
 const port = process.env.PORT || 4000;
 
