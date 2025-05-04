8566862bf1b503286ace15e938f6a02f374cb024
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-04 20:53:51 +0300

    feat: user model and register controller

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,33 @@
+import User from "../models/User.model.js";
+import { StatusCodes } from "http-status-codes";
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      res.status(StatusCodes.BAD_REQUEST).json({ message: "Missing fields" });
+      return;
+    }
+
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        error: true,
+        message: `User with email: ${email}, already exist`,
+      });
+      return;
+    }
+
+    const user = await User.create({ username, email, password });
+
+    res.status(StatusCodes.CREATED).json({ user, token });
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: true, message: `Error during register: ${error}` });
+  }
+};
+const login = async (req, res) => {};
+
+export { register, login };
