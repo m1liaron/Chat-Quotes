af68f7bd7596086d24bf0046ff8b6c268089ce05
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-04 22:22:49 +0300

    feat: createChat controller

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,7 +1,20 @@
 import { StatusCodes } from "http-status-codes";
+import Chat from "../models/Chat.model";
 
 const createChat = async (req, res) => {
     try {
+        const { firstName, lastName } = req.body;
+        if (!firstName?.trim() || !lastName?.trim()) {
+          return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({
+              error: true,
+              message: "firstName and lastName are required.",
+            });
+        }
+
+        const newChat = await Chat.create({ firstName, lastName });
+        res.status(StatusCodes.OK).json(newChat);
     } catch (error) {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
