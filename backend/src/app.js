6451e23cefe68d6a789196930b9f46935475dce5
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-06 12:46:07 +0300

    feat: create websocket server and test it

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,46 @@
 import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws"
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectMongoDb } from "./db/connectMongoDb.js";
 import { validateEnvVariables } from "./helpers/validateEnvVariables.js";
 import { registerRoutes } from "./helpers/helpers.js";
-const app = express();
 
+const app = express();
+const server = http.createServer(app);
+
+dotenv.config();
 app.use(express.json());
 app.use(cors());
-dotenv.config();
 
 registerRoutes(app);
 
 const port = process.env.PORT || 4000;
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected via WebSocket");
+  
+  ws.on("message", (message) => {
+    console.log("Received:", message.toString());
+
+    ws.send(`Server received: ${message}`);
+  });
+
+ ws.on("close", () => {
+   console.log("Client disconnected");
+ });
+});
 
 (async () => {
    try {
       
       validateEnvVariables();
       await connectMongoDb();
-      app.listen(port, () => {
-        console.log(`Server running on port: ${port}...`);
+      server.listen(port, () => {
+        console.log(`HTTP + WebSocket server running on port: ${port}...`);
       });
     } catch(error) {
         console.error("Error starting server: ", error);
