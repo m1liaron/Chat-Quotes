089482afbe733a75b1c39342f551c4d63111ae46
Vlad <machinker34@gmail.com>
2025-05-07 18:16:52 +0300

    feat: connect socket client

---
+++ frontend/src/common/types/Message.ts
@@ -0,0 +1,9 @@
+
+
+type Message = {
+    _id: string;
+    text: string;
+    time: string;
+}
+
+export { type Message };
--- frontend/src/components/ChatWindow/ChatWindow.tsx
+++ frontend/src/components/ChatWindow/ChatWindow.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ChatWindow.css";
 import { MessageBubble } from "../MessageBubble/MessageBubble";
+import { Message } from "../../common/types/Message";
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket;
 
 const ChatWindow: React.FC = () => {
+    const [messages, setMessages] = useState<Message[]>([
+        {
+            _id: "gfgfdg",
+            text: "Hi, how are you?",
+            time: "8/17/2022, 7:43 AM",
+        },
+        {
+            _id: "gfgfdg35dwet",
+            text: "How was your meeting?",
+            time: "8/17/2022, 7:45 AM"
+        }
+    ]);
+    const [inputValue, setInputValue] = useState("");
+
+    useEffect(() => {
+        socket = io("http://localhost:3000");
+
+        socket.on("connect", () => {
+            console.log("Connected to socket server");
+        });
+
+        socket.on("receiveMessage", (data: { text: string; time?: string; _id?: string }) => {
+            console.log("Received message from server:", data);
+            const newMessage: Message = {
+                _id: data._id || Date.now().toString(),
+                text: data.text,
+                time: data.time || new Date().toLocaleString()
+            };
+            setMessages((prevMessages) => [...prevMessages, newMessage]);
+        });
+
+          return () => {
+            socket.disconnect();
+          };
+    }, []);
+ 
     return (
         <div className="chat-window">
             <div className="chat-header">
@@ -10,14 +50,14 @@ const ChatWindow: React.FC = () => {
                 <span>Alice Freeman</span>
             </div>
             <div className="messages">
-                <MessageBubble text="Hi, how are you?" time="8/17/2022, 7:43 AM" left />
-                <MessageBubble
-                    text="Not bad. What about you?"
-                    time="8/17/2022, 7:45 AM"
-                />
-                <MessageBubble
-                    text="How was your meeting?"
-                    time="8/17/2022, 7:48 AM"
+                {messages.map((message) => <MessageBubble text={message.text} time={message.time}/>)}
+            </div>
+            <div className="chat-input">
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Type a message..."
                 />
             </div>
         </div>
