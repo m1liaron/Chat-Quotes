d3e447c61243a86733b8cf3ed8a4043c1a145d76
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-08 16:29:52 +0300

    feat: user have many cahts

--- backend/src/models/Chat.model.js
+++ backend/src/models/Chat.model.js
@@ -6,7 +6,10 @@ const ChatSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    userId: { type: String, required: true, unique: true },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+     },
   },
   {
     timestamps: true,
