4d780e81a4150212af4d0eccbbf21358f83ad982
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-08 18:04:40 +0300

    feat: protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,18 @@ import {
 } from "react-router-dom";
 import { AppPath } from "./common/app/AppPath";
 import { MainPage, RegisterPage, LoginPage } from "./pages/pages";
+import { ProtectedRoute } from "./navigation/ProtectedRoute/ProtectedRoute";
 
 const App: React.FC = () => {
 
   return (
     <Router>
       <Routes>
-        <Route path={AppPath.Root} element={<MainPage/>} />
+        <Route path={AppPath.Root} element={
+          <ProtectedRoute>
+            <MainPage/>
+          </ProtectedRoute>
+        } />
         <Route path={AppPath.Register} element={<RegisterPage/>} />
         <Route path={AppPath.Login} element={<LoginPage/>} />
       </Routes>
--- frontend/src/navigation/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/navigation/ProtectedRoute/ProtectedRoute.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useUser } from "../../contexts/UserProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import { AppPath } from "../../common/app/AppPath";
 
-const ProtectedRoute: React.FC = () => {
-    return (
-        <></>
-    )
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const token = localStorage.getItem("token");
+    const { setUser } = useUser();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (!token) setUser(null);
+    }, [token, setUser]);
+
+    if (!token) {
+        return <Navigate to={AppPath.Login} state={{ from: location }} replace />;
+    }
+
+    return <>{children}</>
 }
 
 export { ProtectedRoute };  
